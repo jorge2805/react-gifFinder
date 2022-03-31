5c2a9b3c4668d40eb97f45a76963bca8888f2788
Jorge Cuevas <20152704@itla.edu.do>
2022-03-30 20:26:45 -0400

    Adding test to useFetchGifs

--- src/test/hooks/useFetchGifs.test.js
+++ src/test/hooks/useFetchGifs.test.js
@@ -4,12 +4,25 @@ import { useFetchGifs } from '../../hooks/useFetchGifs'
 
 describe('Pruebas en useFetchGifs', () => {
 
-    test('debe traer el estado inicial', () => {
-        const { result } = renderHook( () => useFetchGifs('FMAB') );
+    test('debe traer el estado inicial', async() => {
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs('FMAB') );
         const {data, loading} = result.current;
+
+        await waitForNextUpdate();
 
         expect(data).toEqual([]);
         expect(loading).toBe(true);
+    })
+
+    test('Debe retornar arreglo de 10 elementos', async() => {
+
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs('Dragon Ball') );
+        await waitForNextUpdate();
+        
+        const {data, loading} = result.current;
+
+        expect(data.length).toBe(10);
+        expect(loading).toBe(false);
     })
 
 })
